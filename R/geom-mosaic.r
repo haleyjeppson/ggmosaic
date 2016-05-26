58de9f5cb3996c9ca607130cba898f5e2deae53c
Heike Hofmann <hofmann@iastate.edu>
2016-05-26 11:26:02 -0500

    mosaic plot data is correctly calculated

--- R/geom-mosaic.r
+++ R/geom-mosaic.r
@@ -3,12 +3,15 @@
 #' @examples
 #' data(Titanic)
 #' titanic <- as.data.frame(Titanic)
-#' # ggplot(data=titanic) + geom_mosaic()
+#' # library(plyr)
+#' # ggplot(data=titanic) + geom_mosaic(aes(weight=Freq))
+#' # ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=list(Class, Survived))) # only works with modified check_aesthetics
+#' ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=interaction(Class, Survived), group=1))
 #' df <- read.csv("inst/mosaic-rects.csv")
-#' ggplot() + geom_mosaic(aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), data=df)
+#' ggplot() + geom_mosaic(aes(x = xmin, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), data=df)
 
 geom_mosaic <- function(mapping = NULL, data = NULL, stat = "mosaic",
-  position = "dodge", na.rm = FALSE,
+  position = "identity", na.rm = FALSE,
   show.legend = NA, inherit.aes = FALSE, ...)
 {
   ggplot2::layer(
@@ -46,6 +49,21 @@ GeomMosaic <- ggplot2::ggproto("GeomMosaic", ggplot2::Geom,
 #       GeomSegment$draw_panel(medians, panel_scales, coord)
 #     ))
   },
+
+check_aesthetics = function(x, n) {
+  ns <- vapply(x, length, numeric(1))
+  good <- ns == 1L | ns == n
+
+  if (all(good)) {
+    return()
+  }
+  browser()
+  stop(
+    "Aesthetics must be either length 1 or the same as the data (", n, "): ",
+    paste(names(!good), collapse = ", "),
+    call. = FALSE
+  )
+},
 
   draw_key = ggplot2::draw_key_rect
 )
--- R/stat-mosaic.r
+++ R/stat-mosaic.r
@@ -1,14 +1,32 @@
+#' @export
+scale_type.quoted <- function(x) {
+  "quoted"
+}
 
 
 "%||%" <- function(a, b) {
   if (!is.null(a)) a else b
 }
 
+in_data <- function(data, variable) {
+  length(intersect(names(data), variable)) > 0
+}
+
+#' better leave this an interval helper function
+expand_variable <- function(data, variable) {
+  if (!in_data(data, variable)) return()
+
+  split_this <- as.character(data[,variable])
+  df <-   plyr::ldply(strsplit(split_this, split=".", fixed=TRUE), function(x) x)
+  names(df) <- paste(variable, 1:ncol(df), sep="")
+  df
+}
+
 
 #' @rdname geom_mosaic
 #' @export
 stat_mosaic <- function(mapping = NULL, data = NULL, geom = "mosaic",
-  position = "dodge", na.rm = TRUE, show.legend = NA,
+  position = "identity", na.rm = TRUE, show.legend = NA,
   inherit.aes = TRUE, ...)
 {
   ggplot2::layer(
@@ -26,26 +44,71 @@ stat_mosaic <- function(mapping = NULL, data = NULL, geom = "mosaic",
   )
 }
 
-#' @export
-scale_type.formula <- function(x) "formula"
 
 #' @export
 StatMosaic <- ggplot2::ggproto("StatMosaic", ggplot2::Stat,
-#  required_aes = c("formula"),
+#  required_aes = c("vars"),
 #  non_missing_aes = "weight",
 
   setup_params = function(data, params) {
     cat("setup_params from StatMosaic\n")
-#    browser()
+    # browser()
 
     params
   },
 
-  compute_group = function(data, scales) {
+  compute_group = function(data, scales, na.rm=FALSE) {
     cat("compute_groups from StatMosaic\n")
-#    browser()
+  #  browser()
+
+
+    vars <- expand_variable(data, "vars")
+#    data <- dplyr::select(data, -vars)
+    data <- data.frame(data, vars)
+
+    conds <- expand_variable(data, "conds")
+    if (! is.null(conds)) {
+      data <- data.frame(data, conds)
+    }
+    if (!in_data(data, "weight")) {
+      data$weight <- 1
+    }
+
+    formula <- paste("weight~", paste(names(vars), collapse="+"))
+    if (! is.null(conds)) {
+      formula <- paste(formula, paste(names(conds), collapse="+"), sep="|")
+    }
+
+    res <- productplots::prodcalc(data, formula=as.formula(formula),
+                    divider = productplots::mosaic(), cascade=0, scale_max = TRUE,
+                    na.rm = na.rm)
+
 
 #    df  is data frame with data that has xmin, xmax, ymin, ymax
-    data
+    res <- dplyr::rename(res, xmin=l, xmax=r, ymin=b, ymax=t)
+    res$group <- unique(data$group)
+    res$PANEL <- unique(data$PANEL)
+    res
   }
 )
+
+#' might need to overwrite check_aesthetics in geom - at the moment this function gets ignored
+#' @export
+check_aesthetics <- function (x, n)
+{
+#  do a recursive check on the length of aesthetics - this will allow a specification of
+#  variables as a list (to allow for arbitrary many variables such as for mosaic plots or
+#  parallel coordinate plots)
+  islist <- vapply(x, is.list, logical(1))
+  lapply(x[islist==TRUE], check_aesthetics, n =n)
+  x <- x[-which(islist)]
+#  end of recursive check
+
+  ns <- vapply(x, length, numeric(1))
+  good <- ns == 1L | ns == n
+  if (all(good)) {
+    return()
+  }
+  stop("Aesthetics must be either length 1 or the same as the data (",
+       n, "): ", paste(names(!good), collapse = ", "), call. = FALSE)
+}
