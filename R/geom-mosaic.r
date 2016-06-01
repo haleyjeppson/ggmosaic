a3a56f9a45e1ea0e5c33442c82465cce3bb0e8b2
Heike Hofmann <hofmann@iastate.edu>
2016-06-01 15:26:21 -0500

    using output from stat_mosaic for labelling

--- R/geom-mosaic.r
+++ R/geom-mosaic.r
@@ -9,7 +9,10 @@
 #' # ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=list(Class, Survived))) # only works with modified check_aesthetics
 #' ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=product(Class, Survived), group=1))
 #' ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=interaction(Class, Survived), group=1, fill=Age))
-#' ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=interaction(Survived, Class), group=1, fill=Age))
+#' gg <- ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=interaction(Survived, Class), group=1, fill=Age))
+#' gg
+#' gg + geom_text(aes(x = (xmin+xmax)/2, y = (ymin+ymax)/2,
+#' label=paste(paste0("Survived: ",vars1),paste0("Class: ",vars2), sep="\n")), data=subset(ggplot_build(gg)$data[[1]], level==2))
 #' # doing the right thing, but we need labelling to make it less confusing
 #' ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=interaction(Class, Survived), conds = Age, group=1))
 #' ggplot(data=titanic) + geom_mosaic(aes(weight=Freq, vars=Class, group=1, conds=Age, fill=Survived))
@@ -53,49 +56,50 @@ geom_mosaic <- function(mapping = NULL, data = NULL, stat = "mosaic",
 }
 
 #' @importFrom grid grobTree
-GeomMosaic <- ggplot2::ggproto("GeomMosaic", ggplot2::Geom,
-                               setup_data = function(data, params) {
-                                 cat("setup_data in GeomMosaic\n")
-                                 #    data$x <- 1
-                                 #    data$y <- 1
-                                 data
-                               },
-                               required_aes = c("xmin", "xmax", "ymin", "ymax"),
-                               default_aes = ggplot2::aes(width = 0.75, linetype = "solid", fontsize=5,
-                                                          shape = 19, colour = NA,
-                                                          size = .1, fill = "grey30", alpha = .8, stroke = 0.1,
-                                                          linewidth=.1),
+GeomMosaic <- ggplot2::ggproto(
+  "GeomMosaic", ggplot2::Geom,
+  setup_data = function(data, params) {
+    cat("setup_data in GeomMosaic\n")
+    #    data$x <- 1
+    #    data$y <- 1
+    data
+  },
+  required_aes = c("xmin", "xmax", "ymin", "ymax"),
+  default_aes = ggplot2::aes(width = 0.75, linetype = "solid", fontsize=5,
+                             shape = 19, colour = NA,
+                             size = .1, fill = "grey30", alpha = .8, stroke = 0.1,
+                             linewidth=.1),
 
-                               draw_group = function(data, panel_scales, coord) {
-                                 cat("draw_group in GeomMosaic\n")
+  draw_group = function(data, panel_scales, coord) {
+    cat("draw_group in GeomMosaic\n")
 
 
-                                 #     ggplot2:::ggname("geom_lvplot", grobTree(
-                                 #       outliers_grob,
-                                 #       GeomRect$draw_panel(box, panel_scales, coord),
-                                 #       GeomSegment$draw_panel(medians, panel_scales, coord)
-                                 #     ))
-                                 #    browser()
+    #     ggplot2:::ggname("geom_lvplot", grobTree(
+    #       outliers_grob,
+    #       GeomRect$draw_panel(box, panel_scales, coord),
+    #       GeomSegment$draw_panel(medians, panel_scales, coord)
+    #     ))
+    #    browser()
 
-                                 if (all(is.na(data$colour))) data$colour <- alpha(data$fill, data$alpha)
+    if (all(is.na(data$colour))) data$colour <- alpha(data$fill, data$alpha)
 
-                                 GeomRect$draw_panel(subset(data, level==max(data$level)), panel_scales, coord)
-                               },
+    GeomRect$draw_panel(subset(data, level==max(data$level)), panel_scales, coord)
+  },
 
-                               check_aesthetics = function(x, n) {
-                                 ns <- vapply(x, length, numeric(1))
-                                 good <- ns == 1L | ns == n
+  check_aesthetics = function(x, n) {
+    ns <- vapply(x, length, numeric(1))
+    good <- ns == 1L | ns == n
 
-                                 if (all(good)) {
-                                   return()
-                                 }
-                                 # browser()
-                                 stop(
-                                   "Aesthetics must be either length 1 or the same as the data (", n, "): ",
-                                   paste(names(!good), collapse = ", "),
-                                   call. = FALSE
-                                 )
-                               },
+    if (all(good)) {
+      return()
+    }
+    # browser()
+    stop(
+      "Aesthetics must be either length 1 or the same as the data (", n, "): ",
+      paste(names(!good), collapse = ", "),
+      call. = FALSE
+    )
+  },
 
-                               draw_key = ggplot2::draw_key_rect
+  draw_key = ggplot2::draw_key_rect
 )
