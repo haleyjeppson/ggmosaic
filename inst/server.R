90fa6d01d3b0fefbcb865101dd96845e33049f63
Haley Jeppson <hjeppson@iastate.edu>
2016-06-23 14:01:35 -0500

    changed size of plot and added new options

--- inst/server.R
+++ inst/server.R
@@ -7,31 +7,40 @@ shinyServer(function(input, output, session) {
   names<- names(happy)
   d<- data.frame(value=names, label=names)
   updateSelectizeInput(session, 'group', choices = d[c(2,4:9),], server=TRUE, selected=d[2,])
-  updateSelectizeInput(session, 'group2', choices = d[c(2,4:9),], server=TRUE)
+  updateSelectizeInput(session, 'group2', choices = d[c(2,5:9),], server=TRUE)
 
-  output$values<- renderText({
-    input$group
-  })
 
   xstr <- reactive({
-    xx <- paste(input$group, collapse = ",")
+    vars <- rev(input$group)
+    xx <- paste(vars, collapse = ",")
      sprintf("product(%s)", xx)
   })
 
   cond <- reactive({
     if (length(input$group2 > 0)){
-    c <- paste(input$group2, collapse = ",")
-    sprintf("product(%s)", c)
+      conds <- rev(input$group2)
+      c <- paste(conds, collapse = ",")
+      sprintf("product(%s)", c)
     }
     else return()
     })
 
+  plot <- reactive({
+    gg <-  ggplot(data = happy) + geom_mosaic(aes_string( weight = "wtssall", x = xstr(), conds=cond(), fill = input$col ), offset = as.numeric(input$offset))+
+      theme(axis.text.x = element_text(size=rel(1.7), angle = 25, hjust=1), axis.title.x = element_text(size=rel(2.2)),
+            legend.key.size = unit(1, "cm"), legend.text = element_text(size=rel(1.2)),
+            legend.title = element_text(size = rel(1.5)))
+    gg
+  })
 
-  output$mosaicplot = renderPlot({
-     ggplot(data = happy) + geom_mosaic(aes_string( weight = "wtssall", x = xstr(), conds=cond(), fill = input$col ))+
-      theme(axis.text.x = element_text(size=15), axis.title.x = element_text(size=20), legend.key.size = unit(1, "cm"),
-            legend.text = element_text(size=10), legend.title = element_text(size = 15))
-    })
+
+  output$mosaicplot = renderPlot({ plot()   })
+
+  #output$formula<- renderText({
+   # ggplot_build(plot())$data[[1]]["formula"][1,]
+   # vars <- xstr()$vars
+  #  paste("weight ~",  paste(vars, collapse = "+"))
+  #  })
 
 
 })
--- inst/ui.R
+++ inst/ui.R
@@ -12,19 +12,19 @@ shinyUI(fluidPage(
 
     # sidebar with options for controlling parameters
     sidebarPanel(
-      selectizeInput('group', "Select variables to compare:", NULL, multiple = TRUE, options = list(maxItems = 3)),
-      selectizeInput('group2', "Select variables to condition on:", NULL, multiple = TRUE, options = list(maxItems = 2)),
-      uiOutput("values"),
-      selectizeInput(
-        'col', 'Color', names(happy[c(2,4:9)]), selected = "health"
-      ),
+      selectizeInput('group', "Variables to compare:", NULL, multiple = TRUE, options = list(maxItems = 3)),
+      selectizeInput('group2', "Variables to condition on:", NULL, multiple = TRUE, options = list(maxItems = 2)),
+      selectizeInput('col', 'Variable to fill:', names(happy[c(2,4:9)]), selected = "health"),
+      selectizeInput('div', 'Divider', c("mosaic", "ddecker"), selected="mosaic" ),
+      selectizeInput('offset', 'Offset', c(0,0.01,0.025,0.05,0.075,0.1), selected = 0.01),
+      uiOutput("formula"),
       width=3
     ),
 
     # plotting
     mainPanel(
       # textOutput("funct"),
-      plotOutput("mosaicplot"),
+      plotOutput("mosaicplot", height = "700px"),
       width = 9
       )
 
