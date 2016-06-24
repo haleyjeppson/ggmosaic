c0c7450f8e6285da5f6fd7e6e69148fbafd830eb
Haley Jeppson <hjeppson@iastate.edu>
2016-06-24 11:55:13 -0500

    worked on added more features

--- inst/server.R
+++ inst/server.R
@@ -26,20 +26,67 @@ shinyServer(function(input, output, session) {
     })
 
   plot <- reactive({
+    if (input$div == "mosaic"){
     gg <-  ggplot(data = happy) + geom_mosaic(aes_string( weight = "wtssall", x = xstr(), conds=cond(), fill = input$col ), offset = as.numeric(input$offset))+
       theme(axis.text.x = element_text(size=rel(1.7), angle = 25, hjust=1), axis.title.x = element_text(size=rel(2.2)),
             legend.key.size = unit(1, "cm"), legend.text = element_text(size=rel(1.2)),
             legend.title = element_text(size = rel(1.5)))
-    gg
+    }
+    else{
+      if (input$div == "mosaic reversed"){
+        gg <-  ggplot(data = happy) + geom_mosaic(aes_string( weight = "wtssall", x = xstr(), conds=cond(), fill = input$col ),
+                                                  offset = as.numeric(input$offset), divider = mosaic("v"))+
+          theme(axis.text.x = element_text(size=rel(1.7), angle = 25, hjust=1), axis.title.x = element_text(size=rel(2.2)),
+                legend.key.size = unit(1, "cm"), legend.text = element_text(size=rel(1.2)),
+                legend.title = element_text(size = rel(1.5)))
+      }
+      else {
+      gg <-  ggplot(data = happy) + geom_mosaic(aes_string( weight = "wtssall", x = xstr(), conds=cond(), fill = input$col ),
+                                                offset = as.numeric(input$offset), divider=ddecker())+
+        theme(axis.text.x = element_text(size=rel(1.7), angle = 25, hjust=1), axis.title.x = element_text(size=rel(2.2)),
+              legend.key.size = unit(1, "cm"), legend.text = element_text(size=rel(1.2)),
+              legend.title = element_text(size = rel(1.5)))
+      }}
+
+    if (input$coord == TRUE){
+      gg <- gg + coord_flip()
+    }
+    else gg
+
+    if (input$labels == TRUE){
+      create_labels <- function(plot, sub=NA){
+        data=ggplot_build(plot)$data[[1]]
+        # data = subset(data, fill=="grey50")
+        data=subset(data, level==max(data$level))
+        if (!is.na(sub)){
+          data = subset(data, sub)
+        }
+        data$x = (data$xmin+data$xmax)/2
+        data$y = (data$ymin+data$ymax)/2
+        # data$label <- apply(data[grep("^[x]{1,}[0-9]", names(data))],1,paste, collapse = "\n")
+        df <- data.frame(x=data$x, y=data$y, label=data$label)
+        return(df)
+      }
+
+      labs <- create_labels(gg)
+
+      gg <- gg + annotate("text", x = labs$x, labs$y, label=labs$label)
+    }
+    else gg
+
   })
 
 
   output$mosaicplot = renderPlot({ plot()   })
 
-  #output$formula<- renderText({
+ # output$formula<- renderText({
    # ggplot_build(plot())$data[[1]]["formula"][1,]
-   # vars <- xstr()$vars
-  #  paste("weight ~",  paste(vars, collapse = "+"))
+  #  vars <- xstr()
+  #  vars <- gsub("product[:(:]", "", vars)
+  #  vars <- gsub(")", "", vars)
+  #  vars <- gsub(",", " + ", vars)
+
+  #  paste("weight ~",  vars)
   #  })
 
 
--- inst/ui.R
+++ inst/ui.R
@@ -15,9 +15,12 @@ shinyUI(fluidPage(
       selectizeInput('group', "Variables to compare:", NULL, multiple = TRUE, options = list(maxItems = 3)),
       selectizeInput('group2', "Variables to condition on:", NULL, multiple = TRUE, options = list(maxItems = 2)),
       selectizeInput('col', 'Variable to fill:', names(happy[c(2,4:9)]), selected = "health"),
-      selectizeInput('div', 'Divider', c("mosaic", "ddecker"), selected="mosaic" ),
+      selectizeInput('div', 'Divider', c("mosaic", "mosaic reversed", "double decker"), selected="mosaic" ),
       selectizeInput('offset', 'Offset', c(0,0.01,0.025,0.05,0.075,0.1), selected = 0.01),
-      uiOutput("formula"),
+      checkboxInput("coord", label = "Flip coordinates", value = FALSE),
+      checkboxInput("labels", label = "Add labels", value = FALSE),
+
+   #   uiOutput("formula"),
       width=3
     ),
 
@@ -25,6 +28,7 @@ shinyUI(fluidPage(
     mainPanel(
       # textOutput("funct"),
       plotOutput("mosaicplot", height = "700px"),
+      textOutput("formula"),
       width = 9
       )
 
