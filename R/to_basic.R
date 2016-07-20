46ed922d6442f6fcd1559c56fe9e96ae4e1be241
Haley Jeppson <hjeppson@iastate.edu>
2016-07-20 13:18:25 -0500

    deleted unnecessary function

--- R/to_basic.R
+++ R/to_basic.R
@@ -3,13 +3,13 @@ to_basic.GeomMosaic <- function (data, prestats_data, layout, params, p, ...)
 {
   data <- subset(data, level==max(level))
   browser()
-  x.vars<- data[ , grepl("x[0-9]", colnames(data))]
-  x.values <- data.frame(sapply(x.vars, function(x) gsub("-", ": ", x)))
-  x.values[] <- lapply(x.values, as.character)
-  labs <- tidyr::unite_(x.values, col="labs", from=colnames(x.values)[], sep=" /n")
-  fill.vars <- c(gsub(".*<", "<", data$hovertext))
-  all <- data.frame(labs, fill.vars)
-  data$hovertext <- tidyr::unite_(all, col="hovertext", from=colnames(all)[], sep="")
+  # x.vars<- data[ , grepl("x[0-9]", colnames(data))]
+  # x.values <- data.frame(sapply(x.vars, function(x) gsub("-", ": ", x)))
+  # x.values[] <- lapply(x.values, as.character)
+  # labs <- tidyr::unite_(x.values, col="labs", from=colnames(x.values)[], sep=" <br>")
+  # fill.vars <- c(gsub(".*<", "<", data$hovertext))
+  # all <- data.frame(labs, fill.vars)
+  # data$hovertext <- tidyr::unite_(all, col="hovertxt", from=colnames(all)[], sep="")
 
   data$group <- seq_len(nrow(data))
   others <- data[!names(data) %in% c("xmin", "ymin", "xmax",
@@ -30,17 +30,4 @@ to_basic.GeomMosaic <- function (data, prestats_data, layout, params, p, ...)
   prefix_class <- function(x, y) {
     structure(x, class = unique(c(y, class(x))))
   }
-
-
-to_basic.GeomRect <- function(data, prestats_data, layout, params, ...) {
-  data$group <- seq_len(nrow(data))
-  others <- data[!names(data) %in% c("xmin", "ymin", "xmax", "ymax")]
-  data <- with(data, {
-    rbind(cbind(x = xmin, y = ymin, others),
-          cbind(x = xmin, y = ymax, others),
-          cbind(x = xmax, y = ymax, others),
-          cbind(x = xmax, y = ymin, others))
-  })
-  prefix_class(data, "GeomPolygon")
-}
 
