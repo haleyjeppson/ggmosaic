0e1bcc097b5ed9f61d10ae46f9d1fdedbb319649
Haley Jeppson <hjeppson@iastate.edu>
2016-07-20 13:28:26 -0500

    fixed labels

--- R/to_basic.R
+++ R/to_basic.R
@@ -3,13 +3,13 @@ to_basic.GeomMosaic <- function (data, prestats_data, layout, params, p, ...)
 {
   data <- subset(data, level==max(level))
   browser()
-  # x.vars<- data[ , grepl("x[0-9]", colnames(data))]
-  # x.values <- data.frame(sapply(x.vars, function(x) gsub("-", ": ", x)))
-  # x.values[] <- lapply(x.values, as.character)
-  # labs <- tidyr::unite_(x.values, col="labs", from=colnames(x.values)[], sep=" <br>")
-  # fill.vars <- c(gsub(".*<", "<", data$hovertext))
-  # all <- data.frame(labs, fill.vars)
-  # data$hovertext <- tidyr::unite_(all, col="hovertxt", from=colnames(all)[], sep="")
+  x.vars<- data[ , grepl("x[0-9]", colnames(data))]
+  x.values <- data.frame(sapply(x.vars, function(x) gsub("-", ": ", x)))
+  x.values[] <- lapply(x.values, as.character)
+  labs <- tidyr::unite_(x.values, col="labs", from=colnames(x.values)[], sep=" <br>")
+  fill.vars <- c(gsub(".*<", "<", data$hovertext))
+  all <- data.frame(labs, fill.vars)
+  data$hovertext <- unlist(c(tidyr::unite_(all, col="hovertxt", from=colnames(all)[], sep="")))
 
   data$group <- seq_len(nrow(data))
   others <- data[!names(data) %in% c("xmin", "ymin", "xmax",
