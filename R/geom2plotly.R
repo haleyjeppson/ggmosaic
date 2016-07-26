2aa7c4cc8abfb05257b966eddafbcc5114a31a7c
Carson Sievert <cpsievert1@gmail.com>
2016-07-26 17:54:31 -0500

    add weight to hovertext

--- R/geom2plotly.R
+++ R/geom2plotly.R
@@ -1,36 +1,8 @@
 #' @export
-to_basic.GeomMosaic <- function (data, prestats_data, layout, params, p, ...)
-{
-  data <- subset(data, level==max(level))
- # browser()
-  data$hovertext<- gsub("\n", "<br>", data$label)
-  data$hovertext<- gsub("-", ": ", data$hovertext)
-
-  # x.vars<- data[ , grepl("x[0-9]", colnames(data))]
-  # x.values <- data.frame(sapply(x.vars, function(x) gsub("-", ": ", x)))
-  # x.values[] <- lapply(x.values, as.character)
-  # labs <- tidyr::unite_(x.values, col="labs", from=colnames(x.values)[], sep=" <br>")
-  # fill.vars <- c(gsub(".*<", "<", data$hovertext))
-  # all <- data.frame(labs, fill.vars)
-  # data$hovertext <- unlist(c(tidyr::unite_(all, col="hovertxt", from=colnames(all)[], sep="")))
-
-  data$group <- seq_len(nrow(data))
-  others <- data[!names(data) %in% c("xmin", "ymin", "xmax",
-                                     "ymax")]
-  data <- with(data, {
-    rbind(cbind(x = xmin, y = ymin, others), cbind(x = xmin,
-                                                   y = ymax, others), cbind(x = xmax, y = ymax, others),
-          cbind(x = xmax, y = ymin, others))
-  })
-
-  prefix_class(data, "GeomPolygon")
+to_basic.GeomMosaic <- function (data, prestats_data, layout, params, p, ...) {
+  data <- subset(data, level == max(level))
+  data$hovertext <- gsub("\n", "<br>", data$label)
+  data$hovertext <- gsub("-", ": ", data$hovertext)
+  data$hovertext <- paste0(data$hovertext, "<br>Weight: ", data[[".wt"]])
+  to_basic.GeomRect(data)
 }
-
-
- # to_basic.GeomRect <- getFromNamespace("to_basic.GeomRect", asNamespace("plotly"))
-
-
-  prefix_class <- function(x, y) {
-    structure(x, class = unique(c(y, class(x))))
-  }
-
